ab8d8334f4aea7f8547f719b1a75cbf17f296fc6
Alex Faktor <118138704+AlexFaktor@users.noreply.github.com>
2024-02-19 11:52:29 +0200

    foundation

---
+++ Game Life/Game/Game.cs
@@ -0,0 +1,55 @@
+﻿namespace Game_Life.Game
+{
+    public class GameField
+    {
+        public List<List<Cell>>? Field { get; set; }
+
+        public GameField()
+        {
+
+        }
+
+        public void NextGeneration()
+        {
+            var NewGeneration = new List<List<Cell>>();
+
+            // Логіка яка буде створювати нове покоління
+
+
+            Field = NewGeneration;
+        }
+
+        // Правило 1
+        private void RuleOne()
+        { 
+        
+        }
+        // Правило 2
+        private void RuleTwo()
+        {
+
+        }
+
+        // Правило 3
+        private void RuleThree()
+        {
+
+        }
+
+        // Створення ігрового поля
+        private void NewGame()
+        {
+
+        }
+    }
+
+    public class Cell
+    {
+        public bool Alive { get; set; }
+
+        Cell()
+        {
+            Alive = true;
+        }
+    }
+}
